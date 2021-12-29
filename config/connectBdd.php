3b3a824172b69815ab6687f8fdb03e2f71fc0e41
jonael <grusonjonathan@gmail.com>
2021-12-29 02:33:34 +0100

    change bdd informations

--- config/connectBdd.php
+++ config/connectBdd.php
@@ -1,6 +1,6 @@
 <?php   
     //connexion à la base de données
     //(à modifier en fonction de votre base de données dans mon cas la bdd l'appele task1)
-    $bdd = new PDO('mysql:host=localhost;dbname=paranges_sos', 'Joanel','KajoclAdminParanges', 
+    $bdd = new PDO('mysql:host=localhost;dbname=paranges_sos', 'secureuser','KajoclAdminParanges', 
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
