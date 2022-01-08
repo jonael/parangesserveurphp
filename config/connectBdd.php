f83f95628839ca277ac164596046e1092ecb9750
jonael <grusonjonathan@gmail.com>
2022-01-08 21:48:03 +0100

    change headers

--- config/connectBdd.php
+++ config/connectBdd.php
@@ -1,6 +1,6 @@
 <?php   
     //connexion à la base de données
     //(à modifier en fonction de votre base de données dans mon cas la bdd l'appele task1)
-    $bdd = new PDO('mysql:host=127.0.0.1;dbname=paranges_sos', 'root','', 
+    $bdd = new PDO('mysql:host=127.0.0.1;dbname=paranges_sos', 'JoanelAdmin','JoanelAdminParanges', 
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
